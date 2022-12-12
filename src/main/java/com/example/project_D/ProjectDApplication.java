37d6048409646d198d3ff11f8f9a580bee369a16
Jinah <shinjinah@naver.com>
2022-12-12 21:30:01 +0900

    #1. D조 프로젝트 백엔드 기초공사

---
+++ src/main/java/com/example/project_D/ProjectDApplication.java
@@ -0,0 +1,14 @@
+package com.example.project_D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDApplication {
+
+	public static void main(String[] args) {
+		// 톰캣 실행하는 코드
+		SpringApplication.run(ProjectDApplication.class, args);
+	}
+
+}
